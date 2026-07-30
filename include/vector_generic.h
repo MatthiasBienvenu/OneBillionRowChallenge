ef58d0764f067acad65516da620397b554c86b21
Alexis Dauphin <alexis.dauphin91@gmail.com>
2026-07-31 00:34:02 +0200

    Fix ##

--- include/vector_generic.h
+++ include/vector_generic.h
@@ -12,25 +12,25 @@
     } type##_vec;                                                              \
                                                                                \
     /* Intialize the vector. */                                                \
-    int type##_vec##_init(type##_vec *v);                                      \
+    int type##_vec_init(type##_vec *v);                                      \
                                                                                \
     /* Push a value at the end of the vector. */                               \
-    int type##_vec##_push(type##_vec *v, const type *value);                   \
+    int type##_vec_push(type##_vec *v, const type *value);                   \
                                                                                \
     /* Remove the last element from the                                        \
      * vector and copy it to value.                                            \
      * /!\ Does not realloc to a smaller                                       \
      * sized vec but still perform a copy.                                     \
      */                                                                        \
-    int type##_vec##_pop(type##_vec *v, type *dest);                           \
+    int type##_vec_pop(type##_vec *v, type *dest);                           \
                                                                                \
     /* Create a vector from an array.                                          \
      * /!\ Performs a copy of the array.                                       \
      */                                                                        \
-    int type##_vec##_from_array(type##_vec *v, const void *array, size_t len);
+    int type##_vec_from_array(type##_vec *v, const void *array, size_t len);
 
 #define IMPLEMENT_VEC(type)                                                    \
-    int type##_vec##_init(type##_vec *v) {                                     \
+    int type##_vec_init(type##_vec *v) {                                     \
         v->data = malloc(sizeof(type));                                        \
         v->capacity = 1;                                                       \
         v->len = 0;                                                            \
@@ -38,7 +38,7 @@
         return v->data == NULL;                                                \
     }                                                                          \
                                                                                \
-    int type##_vec##_push(type##_vec *v, const type *value) {                  \
+    int type##_vec_push(type##_vec *v, const type *value) {                  \
         if (v->capacity < ++v->len) {                                          \
             v->capacity *= 2;                                                  \
             v->data = realloc(v->data, v->capacity * sizeof(type));            \
@@ -53,7 +53,7 @@
         return 0;                                                              \
     }                                                                          \
                                                                                \
-    int type##_vec##_pop(type##_vec *v, type *dest) {                          \
+    int type##_vec_pop(type##_vec *v, type *dest) {                          \
         if (v->len == 0) {                                                     \
             return 1;                                                          \
         }                                                                      \
@@ -64,7 +64,7 @@
         return 0;                                                              \
     }                                                                          \
                                                                                \
-    int type##_vec##_from_array(type##_vec *v, const void *array,              \
+    int type##_vec_from_array(type##_vec *v, const void *array,              \
                                 size_t len) {                                  \
         v->data = malloc(sizeof(type) * len);                                  \
         memcpy(v->data, array, sizeof(type) * len);                            \
